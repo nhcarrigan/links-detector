4a052fc1107c5e47fe291e1e9a52f9b391bc4a6c
Naomi Carrigan <nhcarrigan@gmail.com>
2024-01-31 07:52:42 -0800

    feat: initial bot scaffold

---
+++ main.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import discord
+import os
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(os.environ.get("BOT_TOKEN"))
