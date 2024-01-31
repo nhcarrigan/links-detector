3844a59f74913543fb6c97a06cd7077c717bf9d7
Naomi Carrigan <nhcarrigan@gmail.com>
2024-01-31 08:11:13 -0800

    feat: bot currently works

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ load_dotenv()
 
 import discord
 import os
+import re
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        if (message.author.bot):
+            return
+        if message.channel.id == 1202280249128386610:
+            print("Found message in right channel")
+            pattern = r'(?i)\b(?:https?://)?(?:www\.)?(?:twitch\.tv|kick\.com)(?:/[^\s]+)?\b'
+            if re.search(pattern, message.content):
+              await message.add_reaction("💜")
+            else:
+              await message.reply("So sorry, but only `twitch.tv` and `kick.com` links are allowed in this channel.")
+              await message.delete()
+              webhook = discord.Webhook.from_url(os.environ.get("WH_URL"), client=self)
+              await webhook.send(content=f"Deleted message for failing link test:\n\n> {message.content}")
 
 intents = discord.Intents.default()
 intents.message_content = True
