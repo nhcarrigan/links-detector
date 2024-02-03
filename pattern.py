452b063958de5c62452e28246eab41f212003283
Mama Naomi <nhcarrigan@gmail.com>
2024-02-03 13:19:00 -0800

    feat: handle multiple links in messages (#8)

--- pattern.py
+++ pattern.py
@@ -1,2 +1,3 @@
 """Regex to check if the string contains a twitch.tv or kick.com link."""
 PATTERN = r'(?i)\b(?:https?://)?(?:www\.)?(?:twitch\.tv|kick\.com)(?:/[^\s]+)\b'
+LINKS = r'(?i)\bhttps?://'
--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import re
 import unittest
 
-from pattern import PATTERN
+from pattern import LINKS, PATTERN
 
 
 class TestPattern(unittest.TestCase):
@@ -35,6 +35,46 @@ class TestPattern(unittest.TestCase):
         """Tests against invalid links."""
         self.assertFalse(re.search(PATTERN, "https://naomi.lgbt"))
         self.assertFalse(re.search(PATTERN, "https://streamcord.io"))
+
+    def test_links(self):
+        """Tests against all links."""
+        self.assertTrue(re.search(PATTERN, "https://kick.com/naomilgbt"))
+        self.assertFalse(re.search(PATTERN, "https://kick.com"))
+        self.assertFalse(re.search(PATTERN, "https://kick.com/"))
+        self.assertTrue(
+            re.search(PATTERN, "Streaming now!\nhttps://kick.com/naomilgbt\nCome join?"))
+        self.assertTrue(re.search(PATTERN, "<https://kick.com/naomilgbt>"))
+        self.assertFalse(
+            re.search(PATTERN, "Streaming now!\nhttps://kick.com/\nCome join?"))
+        self.assertTrue(re.search(PATTERN, "https://twitch.tv/naomilgbt"))
+        self.assertTrue(re.search(PATTERN, "http://twitch.tv/naomilgbt"))
+        self.assertFalse(re.search(PATTERN, "https://twitch.tv"))
+        self.assertFalse(re.search(PATTERN, "https://twitch.tv/"))
+        self.assertTrue(
+            re.search(PATTERN, "Streaming now!\nhttps://twitch.tv/naomilgbt\nCome join?"))
+        self.assertTrue(re.search(PATTERN, "<https://twitch.tv/naomilgbt>"))
+        self.assertFalse(
+            re.search(PATTERN, "Streaming now!\nhttps://twitch.tv/\nCome join?"))
+        self.assertFalse(re.search(PATTERN, "https://naomi.lgbt"))
+        self.assertFalse(re.search(PATTERN, "https://streamcord.io"))
+
+    def test_multiple_links(self):
+        """Tests comparisons of messages with multiple links."""
+        self.assertEqual(len(re.findall(PATTERN, "https://twitch.tv/naomilgbt")),
+                         len(re.findall(LINKS, "https://twitch.tv/naomilgbt")))
+        self.assertEqual(len(re.findall(PATTERN, "https://kick.com/naomilgbt")),
+                         len(re.findall(LINKS, "https://kick.com/naomilgbt")))
+        self.assertLess(len(re.findall(PATTERN, "https://naomi.lgbt")),
+                        len(re.findall(LINKS, "https://naomi.lgbt")))
+        self.assertLess(
+            len(re.findall(PATTERN, "https://twitch.tv/naomilgbt\nhttps://chat.naomi.lgbt")),
+            len(re.findall(LINKS, "https://twitch.tv/naomilgbt\nhttps://chat.naomi.lgbt")))
+        self.assertLess(
+            len(re.findall(
+                PATTERN, "https://twitch.tv/naomilgbt" +
+                "\nhttps://chat.naomi.lgbt\nhttps://kick.com/naomilgbt")),
+            len(re.findall(LINKS, "https://twitch.tv/naomilgbt" +
+                "\nhttps://chat.naomi.lgbt\nhttps://kick.com/naomilgbt")))
 
 
 unittest.main()
